e090fdbdefa921c107ee58b7aa1eeee3bbb26a09
ankitgupta167 <161850130+ankitgupta167@users.noreply.github.com>
2024-06-10 08:37:39 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+// let score = 33
+// let star="33asmaswd" // inside a double code always a string .
+// let don=null
+// let under=undefined
+// let boo=true;
+
+// let arr="ankit"
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof(star)) 
+
+// // converting star in number;
+// let valueInNumber = Number(star);
+// console.log(valueInNumber); // NaN
+// console.log(typeof valueInNumber) // number
+// // converting ansOfNull into number
+// let ansOfNull=Number(don)
+// console.log( typeof don) // object
+// console.log( ansOfNull)// 0
+
+// let ansOfUndfin=Number(under);
+// console.log(under); // undefined
+// console.log(ansOfUndfin) // NaN
+
+// let ansBoo=Number(boo)
+// console.log( boo); // true
+// console.log(ansBoo) // 1
+// // converting arr into number;
+// let ansarr=Number(arr);
+// console.log(arr); // ankit
+// console.log(ansarr) // NaN
+
+// // conclusion of converting into number is 
+// // "33"=>33
+// // ankit => NaN
+// // "33abasrg" => NaN
+// // true => 1
+// // false => 0
+
+let someNumber= 33;
+let stringNumber= String(someNumber)
+console.log(someNumber) //33
+console.log( stringNumber);// 33
+console.log(typeof stringNumber); // string
+
